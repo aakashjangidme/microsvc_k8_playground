1c8f22a3f604028b660c5db3e0db8d44f7da95ff
Aakash Jangid <ajangid25@gmail.com>
2021-06-26 15:27:31 +0530

    Initial react project up and runing with user-service and communicating with clusterIp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { isLoading, error, data } = useQuery("fetchHealth", () =>
+    axios.get("http://tickets.lol/users")
   );
+
+  console.log(`data : ${data?.data.message}`);
+
+  if (isLoading) return <div> Loading... </div>;
+
+  if (error) return <div> "An error has occurred: " + {error}</div>;
+
+  return <h1>{data?.data.message}</h1>;
 }
 
 export default App;
